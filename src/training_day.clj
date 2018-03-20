b5eccf33968edf5006c1bbd829a50545b71847a2
Henrik Vesterinen <henrikv@iki.fi>
2018-03-20 21:54:57 +0200

    First chapter exercices
    Signed-off-by: Henrik Vesterinen <henrikv@iki.fi>

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square
+  "Calculate square"
+  [x]
+  (* x x))
 
-(defn average [a b]
-  ":(")
+(defn average
+  "Average two numbers"
+  [a b]
+  (/ (+ a b) 2)
+  )
